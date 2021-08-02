9d908fec18f557fb251d61b6d10bb6fe919e978e
Robert Burrows <robertb400@comcast.net>
2021-08-02 11:49:22 -0700

    first commit prework

---
+++ prework.py
@@ -0,0 +1,35 @@
+#def hello_username(user_name):
+#   print("Hello" + user_name.upper() + "!")
+#hello_username(' robert')
+
+
+
+
+#oddnumbers = list(range(1,101,2))
+#print(oddnumbers)
+
+
+
+
+#def max_num_in_list (a_list):
+#    return f"max_num_in_list, {a_list}"
+#a_list = ("1,2,9,3,6,5")
+#print (max(a_list))
+
+
+
+
+#def is_leap_year (a_year):
+
+
+
+
+
+
+def is_consecutive (a_list):
+    a_list = input("a list")
+if input == str(range(1,100,1)):
+      print ("is consecutive")
+else:
+     input != str(range(1,100,1)):
+      print ("this is not consecutive")
