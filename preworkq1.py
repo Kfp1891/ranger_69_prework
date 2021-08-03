ce99f02a8bf373402eb3f6d0e5106ab393e724b9
Kfp1891 <87041747+Kfp1891@users.noreply.github.com>
2021-08-03 06:00:24 -0700

    Delete preworkq1.py

--- preworkq1.py
+++
@@ -1,6 +0,0 @@
-#Write a function to print "hello_USERNAME!" USERNAME is the input of the function. 
-#The first line of the code has been defined as below.
-def hello_username (username):
-        return
-username=str(input("enter username : "))
-print(f"Hello " + str(username))
